cbbac2114f81cfa63aa33a8f8ffb3e6c5d4f883b
TheDigitalCrazyPainter <gilles@triptyk.eu>
2015-07-01 10:55:26 +0200

    change folders

---
+++ www/default/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Test</title>
+</head>
+<body>
+  <h1>Welcome to Flucton paradise</h1>
+  <section>
+    <?php phpinfo(); ?>
+  </section>
+</body>
+</html>
