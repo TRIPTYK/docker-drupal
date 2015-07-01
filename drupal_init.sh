4fb1544009d0be77eb4faa98922e544b0c82ad3f
TheDigitalCrazyPainter <gilles@triptyk.eu>
2015-07-01 12:24:30 +0200

    update install script

--- drupal_init.sh
+++ drupal_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 drush dl drupal
-mv -f drupal-*/* ./www/default
+mv -f drupal-*/* drupal-*/.* ./www/default
 cd ./www/default
 drush si standard --db-url=mysql://admin:triptyk@192.168.59.103:3306/triptykDb --db-su=admin --db-su-pw=triptyk --site-name="Test Site";
 cd ../../
